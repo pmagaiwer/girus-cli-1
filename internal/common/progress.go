dc21f9d2661ab28d8d0a9dd4eaad407e93a53d49
Ricardo Silva <ricardolopes18@yahoo.com.br>
2025-08-08 16:59:33 +0200

    feat: adicionar verificação e criação de diretório ao salvar progresso em YAML

--- internal/common/progress.go
+++ internal/common/progress.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gopkg.in/yaml.v3"
 	"os"
+	"path/filepath"
 )
 
 var _, homeDir = os.UserHomeDir()
@@ -46,9 +47,15 @@ func (p *Progress) SetLabStatus(labName string, status string) error {
 // SaveProgressToFile salva o progresso atual em um arquivo YAML na mesma pasta do girus-cli
 func (p *Progress) SaveProgressToFile() error {
 	// Converte o struct Progress para YAML
-	data, err := yaml.Marshal(filePath)
+	data, err := yaml.Marshal(p.Labs)
 	if err != nil {
 		return fmt.Errorf("erro ao converter progresso.yaml para YAML: %w", err)
+	}
+
+	// Verifica se o diretorio do girus-cli já existe
+	dir := filepath.Dir(filePath)
+	if err = os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("erro ao criar o diretorio do girus-cli: %w", err)
 	}
 
 	// Verifica se o arquivo progresso.yaml já existe
@@ -58,6 +65,11 @@ func (p *Progress) SaveProgressToFile() error {
 		if err != nil {
 			return fmt.Errorf("erro ao criar o arquivo progresso.yaml: %w", err)
 		}
+	}
+
+	// Se existir, atualiza o arquivo com os dados do struct Progress
+	if err := os.WriteFile(filePath, data, 0644); err != nil {
+		return fmt.Errorf("erro ao atualizar o arquivo progresso.yaml: %w", err)
 	}
 
 	return nil
